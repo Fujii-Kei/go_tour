20d4b53f4f5b58e5512aeb53c5eabd3ed60f93d8
fujiikei <azyuyu@gmail.com>
2025-02-03 15:49:21 +0900

    i imaginary literals

--- sandbox.go
+++ sandbox.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	v := 42.195 // float64
-	fmt.Printf("v is of type %T\n", v)
+	i := 42           // int
+	f := 3.142        // float64
+	g := 0.867 + 0.5i // complex128
+	fmt.Printf("i is of type %T\n", i)
+	fmt.Printf("f is of type %T\n", f)
+	fmt.Printf("g is of type %T\n", g)
 }
